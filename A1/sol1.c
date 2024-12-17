e0f753c8d7a699ff7288af0512c8c55beb2a56de
William Luu <ltvi.pb@gmail.com>
2024-12-17 23:43:43 +0700

    commit

---
+++ A1/sol1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <limits.h>
+
+int get_min(int* arr, int arr_size)
+{
+    if(arr_size == 0)
+        return -1;
+
+    int min = INT_MAX;
+    
+    for(int i = 0; i < arr_size; i++)
+    {
+        if(*(arr + i) < min)
+            min = *(arr + i);
+    }
+
+    return min;
+}
+
+int get_max(int* arr, int arr_size)
+{
+    if(arr_size == 0)
+        return -1;
+
+    int max = INT_MIN;
+
+    for(int i = 0; i < arr_size; i++)
+    {
+        if(*(arr + i) > max)
+            max = *(arr + i);
+    }
+
+    return max;
+}
+int main()
+{
+    int arr[] = {22, 14, 8, 17, 35, 3};
+    int size = sizeof(arr)/sizeof(*(arr + 0));
+
+    printf("Minimum element is: %d", get_min(arr, size));
+    printf("Maximum element is: %d", get_max(arr, size));
+    return 0;
+}
+
+
+
+
